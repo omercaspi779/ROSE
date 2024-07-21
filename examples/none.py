0d61762d8a4f921c21fcce768b5eca2108d05b70
user <caspi779@gmail.com>
2024-07-21 14:28:14 +0300

    start

--- examples/none.py
+++ examples/none.py
@@ -5,6 +5,13 @@ from rose.common import obstacles, actions  # NOQA
 
 driver_name = "No Driver"
 
-
 def drive(world):
+    x = world.car.x
+    y = world.car.y
+
+    try:
+        obstacle = world.get((x, y - 1))
+    except IndexError:
+        pass
+
     return actions.NONE
